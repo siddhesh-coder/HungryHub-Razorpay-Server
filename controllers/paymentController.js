25741c48abb9ffd5bc24111b085c22913fb145a0
Siddhesh <bhosalesiddhesh2000@gmail.com>
2024-03-17 22:14:43 +0530

    checking payment stat

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -33,7 +33,7 @@ export const paymentVerification = async (req, res) => {
 
     if (paymentIsAuthentic) {
       res.redirect(
-        `${process.env.CLIENT_URL}/paymentSuccess?reference=${razorpay_payment_id}`
+        `https://${process.env.CLIENT_URL}/paymentSuccess?reference=${razorpay_payment_id}`
       );
     } else {
       res
